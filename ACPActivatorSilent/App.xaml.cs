b093c615055ff792e9457a805622b6759615e7c1
vikram <37242754+v-i-k-r-a-m@users.noreply.github.com>
2018-12-12 05:36:19 +0530

    implement activator
    Signed-off-by: vikram <37242754+v-i-k-r-a-m@users.noreply.github.com>

---
+++ ACPActivatorSilent/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ACPActivatorSilent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Activator act = new Activator();
+            act.Activate();
+            this.Shutdown();
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
+:exclamation: **For learning purpose only**    
+
 # ACPActivator
 Program4Pc Audio Converter Pro Activator 
